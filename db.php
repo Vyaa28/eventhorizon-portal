02ace4f0cdabe8616616b7760f135b562c275298
Vyaa28 <vyathivya28@gmail.com>
2025-07-05 17:09:55 +0800

    "Deleted echo"

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ $password   = "Password@123";
 try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully.";
+   
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
