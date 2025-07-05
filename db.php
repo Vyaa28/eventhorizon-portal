26b99a9d981b0cd3c989be771a07e983d81ea761
Vyaa28 <vyathivya28@gmail.com>
2025-07-05 15:36:47 +0800

    "Chnages in db"

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-// db.php
 $serverName = "tcp:eventhorizon-sqlserver-thivya.database.windows.net,1433";
 $database   = "eventhorizon";
 $username   = "sqladmin";
@@ -8,8 +7,8 @@ $password   = "Password@123";
 try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully.";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-    exit;
 }
 ?>
